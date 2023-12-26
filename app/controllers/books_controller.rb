c15a52b759d5d06c9d14b34f0e3487d670c727df
itoriko <127394855+itoriko@users.noreply.github.com>
2023-12-26 14:25:40 +0000

    課題３最終調整後

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,62 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @books = Book.all
+    @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
+  root to: "home#top"
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :users, only: [:show, :edit, :update, :index]
+
+  get 'home/about' => 'home#about'
 end
